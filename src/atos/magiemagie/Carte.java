81eda228b73489e8ef05aba0ab7c922cca202fde
Administrateur <Administrateur@GSVJ.formation.local>
2018-06-14 11:39:23 +0200

    mapping OK.

---
+++ src/atos/magiemagie/Carte.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atos.magiemagie;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Administrateur
+ */
+@Entity
+public class Carte implements Serializable {
+
+    public enum TypeCarte{
+        CORNE_LICORNE,
+        BAVE_CRAPAUD,
+        LAPIS_LAZULI,
+        MANDRAGORE,
+        CHAUVE_SOURIS
+    }
+    
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @ManyToOne
+    @JoinColumn
+    private Joueur joueur;
+    
+    @Enumerated(EnumType.STRING)
+    private TypeCarte typeCarte;
+    
+    
+    public Long getId(String Joueur) {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Carte)) {
+            return false;
+        }
+        Carte other = (Carte) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "magiemagie.Carte[ id=" + id + " ]";
+    }
+    
+}
